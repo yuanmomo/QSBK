0860afed1748ca24c6b620e579f980bb92bea1be
yuanmomo <342398690@qq.com>
2014-02-27 14:01:40 +0800

    去掉所有代码

--- src/main/java/net/yuanmomo/util/FinalString.java
+++
@@ -1,7 +0,0 @@
-package net.yuanmomo.util;
-
-public class FinalString {
-	public static String LOGIN_FAILED="Login failed!";
-	public static String GET_REQUEST="GET";
-	public static String POST_REQUEST="POST";
-}
